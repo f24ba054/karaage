f8ec9bbd4efb0304bfdb8734eef4285fa8aa7f40
f24ba054 <f24ba054@chuo.ac.jp>
2025-06-03 15:33:05 +0900

    3-17

--- karaage/lib/main.dart
+++ karaage/lib/main.dart
@@ -68,12 +68,28 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void buttonPressed(){
-    showDialog(
-        context: context,
-        builder: (BuildContext context) => AlertDialog(
-          title: Text("Hello!"),
-          content: Text("This is sample."),
+  showDialog(
+    context: context,
+    builder: (BuildContext context) => AlertDialog(
+      title: Text("Hello!"),
+      content: const Text("This is sample."),
+      actions: <Widget>[
+        TextButton(
+            child: const Text('Cancel'),
+            onPressed: () => Navigator.pop<String>(context, 'Cancel')
+        ),
+        TextButton(
+            child: const Text('OK'),
+            onPressed: () => Navigator.pop<String>(context, 'OK')
         )
-    );
+      ],
+    ),
+  ).then<void>((value) => resultAlert(value));
+}
+
+void resultAlert(String value) {
+  setState((){
+    _message = 'selected: $value';
+    });
   }
 }
