aae821716ecc17c5f1e792457b22781a7a7bddd9
f24ba054 <f24ba054@chuo.ac.jp>
2025-06-04 15:15:26 +0900

    4-6

--- karaage/lib/main.dart
+++ karaage/lib/main.dart
@@ -3,96 +3,88 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
+      title: 'Generated App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xff2196f3),
+        canvasColor: const Color(0xfffafafa),
+      ),
+      home: FirstScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  static var _message = 'ok.';
-
+// １つ目のスクリーン
+class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('App Name'),
+        title: const Text('Home'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(20.0),
-              child: Text(
-                _message,
-                style: TextStyle(
-                  fontSize: 32.0,
-                  fontWeight: FontWeight.w400,
-                  fontFamily: "Roboto"),
-              ),
-            ),
-
-            Padding(
-              padding: EdgeInsets.all(10.0),
-            ),
-
-            Padding(
-              padding: EdgeInsets.all(10.0),
-              child: ElevatedButton(
-                onPressed:buttonPressed,
-              child: Text(
-                  "tap me!",
-                  style: TextStyle(fontSize:32.0,
-                    color: const Color(0xff000000),
-                    fontWeight: FontWeight.w400,
-                    fontFamily: "Roboto"),
-                )
-              )
-            ),
-          ],
+        child: Container(
+          child: const Text('Home Screen',
+              style: const TextStyle(fontSize: 32.0)),
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 1,
+        items: <BottomNavigationBarItem>[
+          const BottomNavigationBarItem(
+            label: 'Home',
+            icon: const Icon(Icons.home, size:32),
+          ),
+          const BottomNavigationBarItem(
+            label: 'next',
+            icon: const Icon(Icons.navigate_next, size:32),
+          ),
+        ],
+        onTap: (int value) {
+          if (value == 1)
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context)=>SecondScreen()),
+            );
+        },
       ),
     );
   }
-
-  void buttonPressed(){
-  showDialog(
-    context: context,
-    builder: (BuildContext context) => SimpleDialog(
-      title: const Text('Select assignment'),
-      children: <Widget>[
-        SimpleDialogOption(
-          onPressed: () => Navigator.pop<String>(context, 'One'),
-          child: const Text('One'),
-        ),
-        SimpleDialogOption(
-          onPressed: () => Navigator.pop<String>(context, 'Two'),
-          child: const Text('Two'),
-        ),
-        SimpleDialogOption(
-          onPressed: () => Navigator.pop<String>(context, 'Three'),
-          child: const Text('Three'),
-        ),
-      ],
-    ),
-  ).then<void>((value) => resultAlert(value));
 }
 
-void resultAlert(String value) {
-  setState((){
-    _message = 'selected: $value';
-    });
+// ２つ目のスクリーン
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Next"),
+      ),
+      body: Center(
+        child: const Text('Next Screen',
+            style:const TextStyle(fontSize: 32.0)),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        items: <BottomNavigationBarItem>[
+          const BottomNavigationBarItem(
+            label: 'prev',
+            icon: const Icon(Icons.navigate_before, size:32),
+          ),
+          const BottomNavigationBarItem(
+            label: '?',
+            icon: const Icon(Icons.android, size:32),
+          ),
+        ],
+        onTap: (int value) {
+          if (value == 0) Navigator.pop(context);
+        },
+      ),
+    );
   }
 }
