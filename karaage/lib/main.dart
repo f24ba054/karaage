a68400e0c04f34c2f10497aad3e0c83842604627
f24ba054 <f24ba054@chuo.ac.jp>
2025-06-04 15:31:41 +0900

    4-9

--- karaage/lib/main.dart
+++ karaage/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
 
   @override
@@ -14,83 +15,67 @@ class MyApp extends StatelessWidget {
         primaryColor: const Color(0xff2196f3),
         canvasColor: const Color(0xfffafafa),
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => FirstScreen(),
-        '/second': (context) => SecondScreen('Second'),
-        '/third': (context) => SecondScreen('Third'),
-      },
+      home: MyHomePage(),
     );
   }
 }
 
-class FirstScreen extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+
+  static const List<Tab> tabs = <Tab>[
+    Tab(text: 'One'),
+    Tab(text: 'Two'),
+    Tab(text: 'Three'),
+  ];
+
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+        vsync: this,
+        length: tabs.length
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
-      ),
-      body: Center(
-        child:const Text('Home Screen',
-          style: const TextStyle(fontSize: 32.0),
+        title: Text('My App'),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: tabs,
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 1,
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            label: 'Home',
-            icon: const Icon(Icons.home),
-          ),
-          BottomNavigationBarItem(
-            label: 'next',
-            icon: const Icon(Icons.navigate_next),
-          ),
-        ],
-        onTap: (int value) {
-          if (value == 1)
-            Navigator.pushNamed(context, '/second');
-        },
+
+      body: TabBarView(
+        controller: _tabController,
+        children: tabs.map((Tab tab) {
+          return createTab(tab);
+        }).toList(),
       ),
     );
   }
-}
 
-class SecondScreen extends StatelessWidget {
-  final String _value;
-  SecondScreen(this._value);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Next"),
-      ),
-      body: Center(
+  Widget createTab(Tab tab) {
+    return Center(
         child: Text(
-          '$_value Screen',
-          style: const TextStyle(fontSize: 32.0),
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0,
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            label: 'prev',
-            icon: const Icon(Icons.navigate_before),
+          'This is "${tab.text}" Tab.',
+          style: const TextStyle(
+            fontSize: 32.0,
+            color: Colors.blue,
           ),
-          BottomNavigationBarItem(
-            label: '?',
-            icon: const Icon(Icons.android),
-          ),
-        ],
-        onTap: (int value) {
-          if (value == 0) Navigator.pop(context);
-          if (value == 1)
-            Navigator.pushNamed(context, '/third');
-        },
-      ),
+        )
     );
   }
 }
